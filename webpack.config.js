7a2a2414868655c56d7abaf597916ccfe36bbfd7
Julian Waller <git@julusian.co.uk>
2017-07-24 23:03:10 +0100

    Initial template. Text fields and flag

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var config = {
+  context: __dirname + '/src', // `__dirname` is root of project and `src` is source
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: __dirname,
+    filename: "./dist/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss|css)$/, //Check for sass or scss file names
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.js$/, //Check for all js files
+        loader: 'babel-loader',
+        query: {
+          presets: [ "babel-preset-es2015" ].map(require.resolve)
+        }
+      },
+    ]
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+  },
+
+  devtool: "eval-source-map", // Default development sourcemap
+};
+
+// Check if build is running in production mode, then change the sourcemap type
+if (process.env.NODE_ENV === "production") {
+  config.devtool = "source-map";
+}
+
+module.exports = config;
