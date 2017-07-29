bab35375d0c374c5f0d504ea3bfbea000f610749
Julian Waller <git@julusian.co.uk>
2017-07-29 09:56:54 +0100

    Add auto deploy

--- build.sh
+++ build.sh
@@ -17,7 +17,16 @@ cp -r ../../dist dist/wwwroot/
 cp appsettings.json dist/
 rm dist/state.json
 
-if ![ -z "$1" ]; then
+if ! [ -z "$1" ]; then
   docker build -t "guildtv/website:xplosion-dev-$1" .
   docker push "guildtv/website:xplosion-dev-$1"
+
+  if ! [ -z "$KUBE_API" ]; then
+    curl -O "https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+
+    sed -i 's/-latest/-'$CI_BUILD_REF'/g' "kube-dev.yaml"
+    ./kubectl --server="$KUBE_API" --token="$KUBE_TOKEN" --insecure-skip-tls-verify=true  --namespace="$KUBE_NAMESPACE" apply -f kube-dev.yaml
+    
+  fi
 fi
