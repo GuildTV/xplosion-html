e4c5f11ba39672fd162a688736dfc338cb2c9164
Julian Waller <git@julusian.co.uk>
2017-09-27 08:43:27 +0100

    Fix deployment image

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ if ! [ -z "$1" ]; then
     curl -O "https://storage.googleapis.com/kubernetes-release/release/$KUBE_VERSION/bin/linux/amd64/kubectl"
     chmod +x kubectl
 
-    sed -i 's/-latest/-'$CI_BUILD_REF'/g' "kube-dev.yaml"
+    sed -i 's/-latest/-'$1'/g' "kube-dev.yaml"
     ./kubectl --server="$KUBE_API" --token="$KUBE_TOKEN" --insecure-skip-tls-verify=true  --namespace="$KUBE_NAMESPACE" apply -f kube-dev.yaml
     
   fi
