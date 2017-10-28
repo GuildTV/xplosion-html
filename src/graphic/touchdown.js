a87add6fa7c0a0c91140a3c41b7307b1364aec7a
Julian Waller <git@julusian.co.uk>
2017-10-28 16:58:25 +0100

    Add touchdown animation

--- src/graphic/touchdown.js
+++ src/graphic/touchdown.js
@@ -1,19 +1,21 @@
 
-const vidDark = document.querySelector('#touchdown-effect-dark');
-const vidLight = document.querySelector('#touchdown-effect-light');
-
 let touchdownTimer = null;
 window.showTouchdown = function(team) { // team = "l" or "r"
   document.body.classList.remove("touchdown-l", "touchdown-r");
-  document.body.classList.add("touchdown-"+team);//, "touchdown-in", "touchdown-ani");
+  document.body.classList.add("touchdown-"+team);
 
   if (touchdownTimer !== null)
     return;
 
-  touchdownTimer = 1; // temp value
-  flareIn.play().catch(e => flareInPlaying());
-  vidDark.currentTime = 0;
-  vidLight.currentTime = 0;
-  vidDark.play(); 
-  vidLight.play();
+  // start css transition
+  document.body.classList.add("touchdown-in", "touchdown-ani");
+  touchdownTimer = setTimeout(function(){
+    document.body.classList.remove("touchdown-in");
+
+    touchdownTimer = setTimeout(function(){
+      document.body.classList.remove("touchdown-ani");
+      touchdownTimer = null;
+    }, 1100); // Wait for reset
+
+  }, 8000); // Duration
 }
